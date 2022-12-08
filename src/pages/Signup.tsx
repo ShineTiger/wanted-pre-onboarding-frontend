49f574a0fa7b280737275f1843291d70624d25b2
ShineTiger <shinetiger.dev@gmail.com>
2022-12-08 12:45:20 +0900

    :sparkles: (API연결)회원가입 폼 제출 기능 구현

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,4 +1,12 @@
+import axios from "axios";
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import instance from "../api/instance";
+
+type ProfileType = {
+  email: string;
+  password: string;
+};
 
 type ProfileValidation = {
   email?: boolean;
@@ -6,16 +14,26 @@ type ProfileValidation = {
 };
 
 const Signup = () => {
+  const [profile, setProfile] = useState<ProfileType>({
+    email: "",
+    password: "",
+  });
   const [validation, setValidation] = useState<ProfileValidation>({
     email: false,
     password: false,
   });
 
+  //유효성검사에 사용되는 정규식표현
   const emailRegex = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-Za-z0-9\-]+/;
   const passwordRegex = /.{8,}/;
 
+  const navigate = useNavigate();
+
+  //유효성검사
   const validationHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
+    setProfile({ ...profile, [name]: value });
+
     if (name === "email") {
       emailRegex.test(value)
         ? setValidation({ ...validation, email: true })
@@ -27,10 +45,25 @@ const Signup = () => {
     }
   };
 
+  const submitSignupForm = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    instance
+      .post("/auth/signup", profile)
+      .then((res) => {
+        if (res.statusText === "Created") {
+          window.confirm("회원가입이 완료되었습니다. 로그인 하시겠습니까?") &&
+            navigate("/login");
+        }
+      })
+      .catch((err) => {
+        alert("오류가 발생했습니다");
+      });
+  };
+
   return (
     <>
       <h2>회원가입</h2>
-      <form>
+      <form onSubmit={submitSignupForm}>
         <input
           type="email"
           id="email"
