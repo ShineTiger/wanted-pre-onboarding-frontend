1c3445c5cc962371226720d0b1e9fde2c2059a9f
ShineTiger <shinetiger.dev@gmail.com>
2022-12-10 17:08:45 +0900

    :construction: 수정토글 함수를 위한 상태  추가

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -6,8 +6,12 @@ interface TodoListsProps {
   updateTodo: (id: number, newTodo: string) => void;
 }
 
-const TodoList = ({ todos, deleteTodo }: TodoListsProps) => {
+const TodoList = ({ todos, deleteTodo, updateTodo }: TodoListsProps) => {
   const [isUpdate, setIsUpdate] = useState(false);
+
+  const handleUpdateToggle = () => {
+    setIsUpdate(!isUpdate);
+  };
 
   return (
     <div>
@@ -19,12 +23,20 @@ const TodoList = ({ todos, deleteTodo }: TodoListsProps) => {
               <input type="checkbox" />
               {isUpdate ? (
                 <>
-                  <input type="button" value="제출" />
+                  <input
+                    type="button"
+                    value="제출"
+                    onClick={() => updateTodo(i.id, i.todo)}
+                  />
                   <input type="button" value="취소" />
                 </>
               ) : (
                 <>
-                  <input type="button" value="수정" />
+                  <input
+                    type="button"
+                    value="수정"
+                    onClick={handleUpdateToggle}
+                  />
                   <input
                     type="button"
                     value="삭제"
